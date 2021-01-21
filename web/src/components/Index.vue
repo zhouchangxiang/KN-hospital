5843b3d6921cfdd84180a28db874e3ba70ce0c2a
lenux-PC\lenux <minwork0926@163.com>
2021-01-21 17:15:08 +0800

    no message

--- web/src/components/Index.vue
+++ web/src/components/Index.vue
@@ -135,11 +135,11 @@
       }
       window.addEventListener('resize', this.getMenuHeight);
       this.getMenuHeight()
-      // if(sessionStorage.getItem("LoginStatus")) {
-      //   this.$store.commit('setUser',sessionStorage.getItem('WorkNumber'))
-      // }else{
-      //   this.$router.push("/login");
-      // }
+      if(sessionStorage.getItem("LoginStatus")) {
+        this.$store.commit('setUser',sessionStorage.getItem('WorkNumber'))
+      }else{
+        this.$router.push("/login");
+      }
     },
     destroyed() {
 
