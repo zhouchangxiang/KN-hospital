62b08f84796e875667f2d43c23a080de9916cba4
HuangXiongjin <1975436224@qq.com>
2021-02-05 10:44:50 +0800

    修改了增量

--- increment_data.py
+++ increment_data.py
@@ -14,20 +14,16 @@ REDIS_TABLENAME = 'data_realtime'
 while True:
     try:
         print('开始写入增量数据')
-        old_date = redis_coon.hget(REDIS_TABLENAME, 'old_date')
-        now_date = datetime.now().strftime('%Y-%m-%d')
-        address = db_session.query(TagDetail).filter_by(Address='COM1.WATER9F.今日累积流量').all()
-        # address = db_session.query(TagDetail).all()
+        # address = db_session.query(TagDetail).filter_by(Address='COM1.WATER9F.今日累积流量').all()
+        address = db_session.query(TagDetail).all()
         for tag in address:
             print(tag.Address)
             data = redis_coon.hget(REDIS_TABLENAME, tag.Address)
-            if tag.Type == '电表' and data != 'None' and data != 'init':
+            if data != 'None' and data != 'init':
                 old_data = redis_coon.hget(REDIS_TABLENAME, tag.Address + '_old')
-                if data == 'None' or old_data == 'None' or data is None or old_data is None:
-                    pass
-                else:
-                    value = float(data) - float(old_data)
-                    if value > 0.0:
+                value = float(data) - float(old_data)
+                if tag.Type == '电表':
+                    if 0.0 < value < 500:
                         db_session.add(
                             IncrementElectricTable(AreaName=tag.AreaName, IncremenValue=str(value), IncremenType='电',
                                                    CollectionDate=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
@@ -36,26 +32,21 @@ while True:
                         redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old', data)
                         redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old_time',
                                         datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                elif tag.Type == '水表':
+                    if data != '0':
+                        if 0.0 < value < 100:
+                            db_session.add(
+                                IncrementWaterTable(AreaName=tag.AreaName, IncremenValue=str(value), IncremenType='水',
+                                                    CollectionDate=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                                                    Address=tag.Address, Unit='m³'))
+                            db_session.commit()
+                            redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old', data)
+                            redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old_time',
+                                            datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
                     else:
-                        pass
-            elif tag.Type == '水表':
-                old_data = redis_coon.hget(REDIS_TABLENAME, tag.Address + '_old')
-                value = float(data) - float(old_data)
-                if value > 0.0:
-                    db_session.add(
-                        IncrementWaterTable(AreaName=tag.AreaName, IncremenValue=str(value), IncremenType='水',
-                                            CollectionDate=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                                            Address=tag.Address, Unit='m³'))
-                    db_session.commit()
-                    redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old', data)
-                    redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old_time',
-                                    datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+                        redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old', '0')
                 else:
                     pass
-            else:
-                old_date = (datetime.now() + timedelta(1)).strftime('%Y-%m-%d')
-                redis_coon.hset(REDIS_TABLENAME, 'old_date', old_date)
-                old_data = redis_coon.hset(REDIS_TABLENAME, tag.Address + '_old', '0.0')
         print('结束写入增量数据')
         time.sleep(600)
     except Exception as err:
